d7ee20efac74760492859d037eb3c1503dfc6272
ahsanhabib98 <ahredoan@gmail.com>
2018-06-15 20:00:26 +0600

    something update

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 EMAIL_USE_TLS = True
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_HOST_USER = 'ahredoan@gmail.com'
-EMAIL_HOST_PASSWORD = 'nvz17xrr'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_PORT = 587
 
 # Application definition
